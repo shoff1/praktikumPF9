3dda07cccbe9250ef84ee0d9f7edfa7b26a1d3c0
shofilabib21@gmail.com <shofilabib21@gmail.com>
2024-06-23 20:38:51 +0700

    first commit

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Employee;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Employee::factory()->count(10)->create();
+        DB::table('employees')->insert([
+            [
+                'firstname' => 'Talitha',
+                'lastname' => 'Aulia',
+                'email'=> 'talithaali88@gmail.com',
+                'age' => 12,
+                'position_id' => 1
+            ],
+            [
+                'firstname' => 'Kwon',
+                'lastname' => 'Geezlyjen',
+                'email'=> 'geezlyjen@gmail.com',
+                'age' => 5,
+                'position_id' => 2
+            ],
+            [
+                'firstname' => 'Son',
+                'lastname' => 'Sukku',
+                'email'=> 'sukku_kamu@gmail.com',
+                'age' => 24,
+                'position_id' => 3
+            ],
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,115 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+// Rute untuk halaman home
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+// Rute untuk halaman profile
+Route::get('profile', ProfileController::class)->name('profile');
+
+// Rute untuk resource employees
+Route::resource('employees', EmployeeController::class);
+
+// Rute untuk halaman welcome
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//ini mulai praktikum modul 9
+Route::get('/local-disk', function() {
+    Storage::disk('local')->put('local-example.txt', 'This is local example
+   content');
+    return asset('storage/local-example.txt');
+   });
+
+Route::get('/public-disk', function() {
+    Storage::disk('public')->put('public-example.txt', 'This is public example
+    content');
+    return asset('storage/public-example.txt');
+   });
+
+//tampilkan isi file lokal-publik
+Route::get('/retrieve-local-file', function() {
+    if (Storage::disk('local')->exists('local-example.txt')) {
+    $contents = Storage::disk('local')->get('local-example.txt');
+    } else {
+    $contents = 'File does not exist';
+    }
+    return $contents;
+   });
+
+Route::get('/retrieve-public-file', function() {
+    if (Storage::disk('public')->exists('public-example.txt')) {
+    $contents = Storage::disk('public')->get('public-example.txt');
+    } else {
+    $contents = 'File does not exist';
+    }
+    return $contents;
+   });
+
+//download file lokal-publik
+Route::get('/download-local-file', function() {
+    return Storage::download('local-example.txt', 'local file');
+   });
+
+Route::get('/download-public-file', function() {
+    return Storage::download('public/public-example.txt', 'public file');
+   });
+
+//tampilkan url path dan size file
+Route::get('/file-url', function() {
+    // Just prepend "/storage" to the given path and return a relative URL
+    $url = Storage::url('local-example.txt');
+    return $url;
+   });
+   Route::get('/file-size', function() {
+    $size = Storage::size('local-example.txt');
+    return $size;
+   });
+   Route::get('/file-path', function() {
+    $path = Storage::path('local-example.txt');
+    return $path;
+   });
+
+//simpan file via form
+Route::get('/upload-example', function() {
+    return view('upload_example');
+   });
+   Route::post('/upload-example', function(Request $request) {
+    $path = $request->file('avatar')->store('public');
+    return $path;
+   })->name('upload-example');
+
+//hapus file di storage
+Route::get('/delete-local-file', function(Request $request) {
+    Storage::disk('local')->delete('local-example.txt');
+    return 'Deleted';
+   });
+   Route::get('/delete-public-file', function(Request $request) {
+    Storage::disk('public')->delete('public-example.txt');
+    return 'Deleted';
+   });
+
+//hapus file di storage
+Route::get('/delete-local-file', function(Request $request) {
+    Storage::disk('local')->delete('local-example.txt');
+    return 'Deleted';
+   });
+
+   Route::get('/delete-public-file', function(Request $request) {
+    Storage::disk('public')->delete('public-example.txt');
+    return 'Deleted';
+   });
+
+   //rute baru upload web data master
+Route::get('download-file/{employeeId}', [EmployeeController::class,
+   'downloadFile'])->name('employees.downloadFile');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
